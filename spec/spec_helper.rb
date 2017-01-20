bf2189431e2e9a58ba0b2a806547744fd4f06dbb
Jere Julian <jere@arista.com>
2017-01-20 15:18:32 -0500

    Remove localhost exception for WebMock.  Add Net::Http hooks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ Dir["#{dir}/support/**/*.rb"].sort.each { |f| require f }
 
 include FixtureHelpers
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(net_http_connect_on_start: true)
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
