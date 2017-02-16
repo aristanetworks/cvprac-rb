eaee31b6a8b0e9667042d19f0d28f4492e918f1b
Jere Julian <jere@arista.com>
2017-02-16 16:57:06 -0500

    Add git pre-commit hook and setup script

---
+++ bin/install-hooks.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+HOOKS=('pre-commit.sh')
+
+for script in ${HOOKS}; do
+  hook=`basename -s '.sh' ${script}`
+  echo -n "    Linking ${hook} hook..."
+  if [ -L .git/hooks/${hook} ]; then
+    echo "already linked."
+  elif [ -f .git/hooks/${hook} ]; then
+    echo -e "\033[31mERROR: exists but is not a link.\033[0m"
+  else
+    ln -s ../../bin/${script} .git/hooks/${hook}
+    echo "complete."
+  fi
+done
