710ba2d24406a8f91f11917d770c9d1f9c72e66c
Jere Julian <jere@arista.com>
2017-04-05 16:15:13 -0400

    Fix shellcheck findings

--- bin/install-hooks.sh
+++ bin/install-hooks.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
 
 HOOKS=('pre-commit.sh')
 
-for script in ${HOOKS}; do
-  hook=`basename -s '.sh' ${script}`
-  echo -n "    Linking ${hook} hook..."
-  if [ -L .git/hooks/${hook} ]; then
-    echo "already linked."
-  elif [ -f .git/hooks/${hook} ]; then
-    echo -e "\033[31mERROR: exists but is not a link.\033[0m"
-  else
-    ln -s ../../bin/${script} .git/hooks/${hook}
-    echo "complete."
-  fi
+for script in "${HOOKS[@]}"; do
+    hook="$(basename -s '.sh' "${script}")"
+    echo -n "    Linking ${hook} hook..."
+    if [ -L ".git/hooks/${hook}" ]; then
+        echo "already linked."
+    elif [ -f ".git/hooks/${hook}" ]; then
+        echo -e "\033[31mERROR: exists but is not a link.\033[0m"
+    else
+        ln -s "../../bin/${script}" ".git/hooks/${hook}"
+        echo "complete."
+    fi
 done
